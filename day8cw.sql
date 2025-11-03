3134bb168c8a94fe92c389ad069ca73047245415
Vaisakh93 <vaisakh56567@gmail.com>
2025-11-03 12:22:28 +0530

    mysql

---
+++ day8cw.sql
@@ -0,0 +1,17 @@
+
+CREATE DATABASE Grocery_Shop;
+
+USE Grocery_Shop;
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    price DECIMAL(10, 2)
+);
+
+ALTER TABLE products
+ADD category VARCHAR(50);
+
+TRUNCATE TABLE products;
+
+DROP DATABASE Grocery_Shop;

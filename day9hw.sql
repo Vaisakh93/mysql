3096e0da50de34e3c273b1129fdcb7c428fabd19
Vaisakh93 <vaisakh56567@gmail.com>
2025-11-04 16:26:50 +0530

    mysql

--- day9hw.sql
+++ day9hw.sql
@@ -8,8 +8,8 @@ INSERT INTO departments (emp_id, department_name) VALUES (1, 'HR'),(2, 'IT'),(4,
 
 SELECT e.id, e.name, d.department_name FROM employees e LEFT JOIN departments d ON e.id = d.emp_id
 
-SELECT e.id, e.name, d.department_name FROM employees e INNER JOIN departments d ON e.id = d.emp_id;
+SELECT e.id, e.name, d.department_name FROM employees e INNER JOIN departments d ON e.id = d.emp_id
 
-SELECT e.id, e.name, d.department_name FROM employees e RIGHT JOIN departments d ON e.id = d.emp_id;
+SELECT e.id, e.name, d.department_name FROM employees e RIGHT JOIN departments d ON e.id = d.emp_id
 
 
